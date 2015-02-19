862ba652392eb89cd7ea1338a95816007a656811
Imthiaz Rafiq <hmimthiaz@imthi.com>
2015-02-19 12:33:47 +0400

    Updated the bundle to use annotations

--- src/Bellwether/BWCMSBundle/Controller/DefaultController.php
+++ src/Bellwether/BWCMSBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace Bellwether\BWCMSBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
     public function indexAction($name)
     {
-        return $this->render('BWCMSBundle:Default:index.html.twig', array('name' => $name));
+        return array('name' => $name);
     }
 }
--- src/Bellwether/BWCMSBundle/DependencyInjection/BWCMSExtension.php
+++ src/Bellwether/BWCMSBundle/DependencyInjection/BWCMSExtension.php
@@ -22,7 +22,7 @@ class BWCMSExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.php');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
