b8846fa350f1dc871e1ec8fc9aaf967acc4c5401
Imthiaz Rafiq <hmimthiaz@imthi.com>
2015-02-26 22:54:22 +0400

    Updates

---
+++ src/Bellwether/BWCMSBundle/Classes/AdminMenuManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bellwether\BWCMSBundle\Classes;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Bellwether\BWCMSBundle\Classes\Base\BaseService;
+
+
+class AdminMenuManager extends BaseService
+{
+
+    function __construct(ContainerInterface $container = null, RequestStack $request_stack = null)
+    {
+        $this->setContainer($container);
+        $this->setRequestStack($request_stack);
+    }
+
+    /**
+     * @return ContentManager
+     */
+    public function getManager()
+    {
+        return $this;
+    }
+
+}
