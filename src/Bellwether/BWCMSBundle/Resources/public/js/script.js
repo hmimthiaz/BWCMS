c76ef327317e4c82233c9ffae0e1048a091cf99d
Imthiaz Rafiq <hmimthiaz@imthi.com>
2015-03-13 23:41:36 +0400

    Content: Work in progress

--- src/Bellwether/BWCMSBundle/Resources/public/js/script.js
+++ src/Bellwether/BWCMSBundle/Resources/public/js/script.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
-    $('button.FCAdd').click(function (e) {
+    $('div.FCHolder div.FCData').children().each(function () {
+        var FCRemoveButton = $('<button type="button" class="btn btn-danger FCRemove"><i class="glyphicon glyphicon-minus"></i></button>');
+        FCRemoveButton.click(function (e) {
+            e.preventDefault();
+            $(this).parent().remove();
+        });
+        $(this).append(FCRemoveButton);
+    });
+    $('div.FCHolder button.FCAdd').click(function (e) {
         e.preventDefault();
         var totalItems = $(this).parent().children().children().length;
         var fData = $(this).parent().find('div.FCData').data('prototype');
         fData = fData.replace(/__name__label__/g, totalItems);
         fData = fData.replace(/__name__/g, totalItems);
         $(this).parent().find('div.FCData').append(fData);
+        var FCRemoveButton = $('<button type="button" class="btn btn-danger FCRemove"><i class="glyphicon glyphicon-minus"></i></button>');
+        FCRemoveButton.click(function (e) {
+            e.preventDefault();
+            $(this).parent().remove();
+        });
+        $(this).parent().find('div.FCData').children(':last').append(FCRemoveButton);
     });
 
 
