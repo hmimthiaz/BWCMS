15573ecbb3d7f4582748683d88e9fd0b491cea63
Imthiaz Rafiq <hmimthiaz@imthi.com>
2015-06-24 01:05:48 +0400

    Content media branch out work in progress..

--- core/Bellwether/BWCMSBundle/Entity/ContentEntity.php
+++ core/Bellwether/BWCMSBundle/Entity/ContentEntity.php
@@ -70,6 +70,11 @@ class ContentEntity
      */
     private $meta;
 
+    /**
+     * @ORM\OneToMany(targetEntity="Bellwether\BWCMSBundle\Entity\ContentMediaEntity", mappedBy="content")
+     */
+    private $media;
+
     private $loadedMeta = null;
 
     /**
@@ -203,6 +208,7 @@ class ContentEntity
     public function __construct()
     {
         $this->meta = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->media = new \Doctrine\Common\Collections\ArrayCollection();
         $this->relation = new \Doctrine\Common\Collections\ArrayCollection();
         $this->scope = ContentScopeType::CPublic;
         $this->sortBy = ContentSortByType::Created;
@@ -221,11 +227,18 @@ class ContentEntity
     /**
      * @return \Doctrine\Common\Collections\ArrayCollection
      */
+    public function getMedia()
+    {
+        return $this->media;
+    }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
     public function getRelation()
     {
         return $this->relation;
     }
-
 
     /**
      * @return mixed
@@ -748,6 +761,26 @@ class ContentEntity
      * @return $this
      */
     public function removeMeta(ContentMetaEntity $meta)
+    {
+        $this->meta->removeElement($meta);
+        return $this;
+    }
+
+    /**
+     * @param ContentMediaEntity $meta
+     * @return $this
+     */
+    public function addMedia(ContentMediaEntity $meta)
+    {
+        $this->meta->add($meta);
+        return $this;
+    }
+
+    /**
+     * @param ContentMediaEntity $meta
+     * @return $this
+     */
+    public function removeMedia(ContentMediaEntity $meta)
     {
         $this->meta->removeElement($meta);
         return $this;
---
+++ core/Bellwether/BWCMSBundle/Entity/ContentMediaEntity.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Bellwether\BWCMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ContentMeta
+ *
+ * 
+ * @ORM\Entity(repositoryClass="Bellwether\BWCMSBundle\Entity\ContentMediaRepository")
+ * @ORM\Table(name="BWContentMedia")
+ */
+class ContentMediaEntity
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\Column(type="guid", name="id")
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Bellwether\BWCMSBundle\Entity\ContentEntity", inversedBy="media")
+     * @ORM\JoinColumn(name="contentId", referencedColumnName="id", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $mime;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $extension;
+
+    /**
+     * @ORM\Column(type="bigint", nullable=true)
+     */
+    private $size;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $height;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $width;
+
+    /**
+     * @ORM\Column(type="blob", nullable=true)
+     */
+    private $data;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param mixed $file
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMime()
+    {
+        return $this->mime;
+    }
+
+    /**
+     * @param mixed $mime
+     */
+    public function setMime($mime)
+    {
+        $this->mime = $mime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+
+    /**
+     * @param mixed $extension
+     */
+    public function setExtension($extension)
+    {
+        $this->extension = $extension;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param mixed $size
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    /**
+     * @param mixed $height
+     */
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    /**
+     * @param mixed $width
+     */
+    public function setWidth($width)
+    {
+        $this->width = $width;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+}
