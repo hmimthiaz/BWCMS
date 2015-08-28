3ce476617b2b704ed549950f3f8a1e02912daee8
Imthiaz Rafiq <hmimthiaz@imthi.com>
2015-08-28 23:05:43 +0400

    Added new function to print email link with javascript

--- core/Bellwether/BWCMSBundle/Classes/Service/TwigService.php
+++ core/Bellwether/BWCMSBundle/Classes/Service/TwigService.php
@@ -122,6 +122,7 @@ class TwigService extends BaseService implements \Twig_ExtensionInterface
             'thumb' => new \Twig_Function_Method($this, 'getThumbImage'),
             'pagination' => new \Twig_Function_Method($this, 'getPagination', array('is_safe' => array('html'))),
             'loc' => new \Twig_Function_Method($this, 'getLocale', array('is_safe' => array('html'))),
+            'emailLink' => new \Twig_Function_Method($this, 'getEmailLink', array('is_safe' => array('html'))),
             'exit' => new \Twig_Function_Method($this, 'doExit', array('is_safe' => array('html'))),
         );
     }
@@ -318,6 +319,28 @@ class TwigService extends BaseService implements \Twig_ExtensionInterface
         return call_user_func_array(array($this->locale(), "get"), func_get_args());
     }
 
+
+    /**
+     * @param string $email
+     * @param string $email
+     * @return string
+     */
+    public function getEmailLink($email)
+    {
+        $character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+        $key = str_shuffle($character_set);
+        $cipher_text = '';
+        $id = 'e' . rand(1, 999999999);
+        for ($i = 0; $i < strlen($email); $i += 1) {
+            $cipher_text .= $key[strpos($character_set, $email[$i])];
+        }
+        $script = 'var a="' . $key . '";var b=a.split("").sort().join("");var c="' . $cipher_text . '";var d="";';
+        $script .= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));';
+        $script .= 'document.getElementById("' . $id . '").innerHTML="<a href=\\"mailto:"+d+"\\">"+d+"</a>"';
+        $script = "eval(\"" . str_replace(array("\\", '"'), array("\\\\", '\"'), $script) . "\")";
+        $script = '<script type="text/javascript">/*<![CDATA[*/' . $script . '/*]]>*/</script>';
+        return '<span id="' . $id . '">[javascript protected email address]</span>' . $script;
+    }
 
     public function doExit()
     {
