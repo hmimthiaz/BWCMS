32a6be07a2d1ee30d48e53779c813acd03059c40
Imthiaz Rafiq <hmimthiaz@imthi.com>
2015-09-08 15:51:23 +0400

    Made sure security is enabled on app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,7 +9,12 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (stripos($_SERVER['HTTP_USER_AGENT'], 'PHPDebug') === false) {
+
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || stripos($_SERVER['HTTP_USER_AGENT'], 'PHPDebug') === false
+) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file.');
 }
