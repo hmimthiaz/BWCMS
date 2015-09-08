1142951d1fcfc3b80e2d84d0b7eea7b274a07bc1
Imthiaz Rafiq <hmimthiaz@imthi.com>
2015-09-08 15:56:14 +0400

    Security restore on debug mode...

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,10 +13,11 @@ use Symfony\Component\Debug\Debug;
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
-    || stripos($_SERVER['HTTP_USER_AGENT'], 'PHPDebug') === false
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file.');
+    if (stripos($_SERVER['HTTP_USER_AGENT'], 'PHPDebug') === false) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file.');
+    }
 }
 
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
