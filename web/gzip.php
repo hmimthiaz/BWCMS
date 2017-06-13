838463579bd6456a27dc3f20cc7b5b1eea38d7d2
Imthiaz Rafiq <imthi@dxb.io>
2017-06-13 13:53:33 +0400

    fixed file disclosure vulnerability

--- web/gzip.php
+++ web/gzip.php
@@ -1,16 +1,27 @@
 <?php
 $baseDIR = __DIR__;
-$requestFile = $baseDIR . DIRECTORY_SEPARATOR .'skins'. $_REQUEST['file'];
+$requestFile = $baseDIR . DIRECTORY_SEPARATOR . 'skins' . $_REQUEST['file'];
+$requestFile = str_replace('..', '', $requestFile);
+$requestFile = str_replace('//', '/', $requestFile);
+$requestFile = str_replace('//', '/', $requestFile);
+$requestFile = str_replace('//', '/', $requestFile);
+$requestFile = str_replace('\\', '\\', $requestFile);
 
 if (!file_exists($requestFile)) {
     header("HTTP/1.0 404 Not Found");
     exit;
 }
-if (preg_match("/\\.(?:css)$/ui", $requestFile)) {
+
+$fileExtension = strtolower(pathinfo($requestFile, PATHINFO_EXTENSION));
+if ($fileExtension=='css') {
     header('Content-Type: text/css');
-} else {
+} else if ($fileExtension=='js') {
     header('Content-Type: application/javascript');
+} else {
+    header("HTTP/1.0 404 Not Found");
+    exit;
 }
+
 header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60 * 24)));
 ob_start("ob_gzhandler");
 @readfile($requestFile);
